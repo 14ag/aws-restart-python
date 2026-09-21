9694ffb632fd9f1625c3ad169d31b29d7c800e4f
Student <student@example.com>
2026-09-21 22:09:22 +0000

    Creating a Hello, World Program

---
+++ hello-world.py
@@ -0,0 +1,5 @@
+# When someone learns how to program, it is traditional to start with the Hello, World program.
+# This simple program verifies that you have installed the Python tools correctily.
+
+
+print("hello, world")
