8469bbd1464b137d8018f6b993870f45ad7b6927
Student <student@example.com>
2026-09-21 22:26:01 +0000

    String concatenation is the process of combining two strings into one string. You have actually been doing string concatenation since lab 1, but you didn’t call this process by that term. The plus sign (+) is used to concatenate strings.

--- string-data-type.py
+++ string-data-type.py
@@ -3,3 +3,10 @@ myString = "This is a string."
 print(myString)
 print(type(myString))
 print(myString + " is of the data type " + str(type(myString)))
+
+
+# string concatenation
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
