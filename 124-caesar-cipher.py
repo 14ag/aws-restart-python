ec47cd8da9658441faebbff8bfaba12cb68f84e0
Student <student@example.com>
2026-09-22 14:27:24 +0000

    The cipher key is how far you will shift the letters. By using two alphabets, you can have a cipher key that is any integer from 1 to 25

--- 124-caesar-cipher.py
+++ 124-caesar-cipher.py
@@ -11,3 +11,8 @@ def getMessage():
     stringToEncrypt = input("Please enter a message to encrypt: ")
     return stringToEncrypt
 
+# Getting a cipher key
+def getCipherKey():
+    shiftAmount = input( "Please enter a key (whole number from 1-25): ")
+    return shiftAmount
+
