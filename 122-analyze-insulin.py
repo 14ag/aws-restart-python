16b7183fb662d6e91f9ebe69f27086ee5e8cf249
Student <student@example.com>
2026-09-22 14:18:00 +0000

    Here, you will use lists, for and while loops, and basic math to calculate the net charge of insulin from pH 0 to pH 14

---
+++ 122-analyze-insulin.py
@@ -0,0 +1,13 @@
+# Python3.11  
+# Coding: utf-8  
+# Store the human preproinsulin sequence in a variable called preproinsulin:  
+preproInsulin = "malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktrreaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"  
+# Store the remaining sequence elements of human insulin in variables:  
+lsInsulin = "malwmrllpllallalwgpdpaaa"  
+bInsulin = "fvnqhlcgshlvealylvcgergffytpkt"  
+aInsulin = "giveqcctsicslyqlenycn"  
+cInsulin = "rreaedlqvgqvelgggpgagslqplalegslqkr"  
+insulin = bInsulin + aInsulin
+
+pKR = {'y': 10.07,'c': 8.18,'c': 8.18,'h': 6.00,'h': 6.00,'d': 3.65,'e': 4.25}
+
