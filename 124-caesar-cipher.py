31cbed7dafff1f9e5199374008880788df7337f3
Student <student@example.com>
2026-09-22 14:26:17 +0000

    The next function you define will request a message to encrypt from the user.

--- 124-caesar-cipher.py
+++ 124-caesar-cipher.py
@@ -5,3 +5,9 @@ def getDoubleAlphabet(alphabet):
     return doubleAlphabet
 
 print(getDoubleAlphabet(ABC))
+
+# Encrypting a message
+def getMessage():
+    stringToEncrypt = input("Please enter a message to encrypt: ")
+    return stringToEncrypt
+
