325a896c098381def582f314b948b1b6d2c3110c
Student <student@example.com>
2026-09-22 12:30:19 +0000

    data cleaning

---
+++ 118-analyze-insulin.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import re
+
+
+# clean data
+INSULIN_FILE=Path(__file__).parents[0] / "preproinsulin-seq.txt"
+
+clean_data=[]
+with open(INSULIN_FILE, "r", encoding="utf-8") as file:
+    insulin_data=file.readlines() #line by line array
+
+    for i in range(0,len(insulin_data)):
+        data=insulin_data[i].strip()
+        
+        #remove leading numbers and surrounding spaces
+        if len(data) > 7:
+            entry=re.sub(r"\d+", "", data)
+            clean_data.append(entry.strip())
+
+for i in clean_data:
+    print(i)
+
+        
